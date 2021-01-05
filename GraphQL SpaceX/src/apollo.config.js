ec02c10412e2391502c6cf491b6af8036b577696
Alan-Rodz <57020015+Alan-Rodz@users.noreply.github.com>
2021-01-04 19:26:18 -0700

    Delete apollo.config.js

--- GraphQL SpaceX/src/apollo.config.js
+++
@@ -1,9 +0,0 @@
-//Esto le dice a Apollo donde encontrar nuestra API backend (que en este caso es la de SpaceX)
-module.exports = {
-    client: {
-        service: {
-            name: 'angular-spacex-graphql-codegen',
-            url:'https://api.spacex.land/graphql/'
-        }
-    }
-};
